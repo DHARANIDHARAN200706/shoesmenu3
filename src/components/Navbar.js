b5a692ff494ee55e62788ffac56aa7b72ea4a091
DHARANIDHARAN200706 <dharanidharanjaidev@gmail.com>
2025-06-10 13:17:44 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
-        <a className="navbar-brand" href="#">HOTPACK</a>
+        <a className="navbar-brand" href="#">URWISH</a>
         <button
           className="navbar-toggler"
           type="button"
